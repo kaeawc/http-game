50c2fceb50250ca973b0d3ab7ee46ca477e7893e
Jason Pearson <jason.d.pearson@gmail.com>
2013-12-19 17:46:31 -0500

    it was determined that all Models would need to look to the Future and talk to Databases

--- app/models/Character.scala
+++ app/models/Character.scala
@@ -1,5 +1,19 @@
 package models
 
+import anorm._
+import anorm.SqlParser._
+
+import play.api.Logger
+import play.api.db.DB
+import play.api.Play.current
+import play.api.libs.json._
+
+import java.util.Date
+
+import scala.concurrent.{Future,ExecutionContext}
+
+import ExecutionContext.Implicits.global
+
 case class Character(
   id   : Long,
   name : String,
